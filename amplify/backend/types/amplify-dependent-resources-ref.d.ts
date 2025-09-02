3c0c0b5ed48ab3f03632e2aa1dc978c447c60111
AbigailAsibreAmoah <abigailamoah3@gmail.com>
2025-09-02 22:34:21 +0000

    Initial commit - setup Amplify project with API and Storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "bucketlist24": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
